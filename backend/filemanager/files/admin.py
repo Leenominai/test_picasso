b1450d83ebb74f21dcfb28a82dab72c72039dd9e
Александр Рассыхаев <farexzon@yandex.ru>
2023-09-21 18:50:42 +0300

    Backend: files app v1.0

---
+++ backend/filemanager/files/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import File
+
+
+@admin.register(File)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('file', 'uploaded_at', 'processed')
+    list_filter = ('processed',)
+    search_fields = ('file',)
