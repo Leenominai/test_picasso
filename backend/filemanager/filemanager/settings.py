c433db6112d08377d1c5aceb1bdbac724330fc66
Александр Рассыхаев <farexzon@yandex.ru>
2023-09-22 12:56:48 +0300

    Backend: upload v2.02

--- backend/filemanager/filemanager/settings.py
+++ backend/filemanager/filemanager/settings.py
@@ -128,8 +128,8 @@ STATIC_ROOT = BASE_DIR / "static"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
